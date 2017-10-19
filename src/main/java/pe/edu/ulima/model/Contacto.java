183187290a941e7aa700f1eea4fb77d0f527c2f4
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-19 18:40:39 -0500

    Agrenado nuevo servlet

--- src/main/java/pe/edu/ulima/model/Contacto.java
+++ src/main/java/pe/edu/ulima/model/Contacto.java
@@ -1,5 +1,60 @@
 package pe.edu.ulima.model;
 
-public class Contacto {
+import java.io.Serializable;
 
+public class Contacto implements Serializable{
+	private Long dni;
+	private String nombre;
+	private String paterno;
+	private String materno;
+	private String email;
+	
+	public Contacto(Long dni, String nombre, String paterno, String materno, String email) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.paterno = paterno;
+		this.materno = materno;
+		this.email = email;
+	}
+	
+	public Long getDni() {
+		return dni;
+	}
+	
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getPaterno() {
+		return paterno;
+	}
+	
+	public void setPaterno(String paterno) {
+		this.paterno = paterno;
+	}
+	
+	public String getMaterno() {
+		return materno;
+	}
+	
+	public void setMaterno(String materno) {
+		this.materno = materno;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
---
+++ src/main/java/pe/edu/ulima/servlets/RegContacto.java
@@ -0,0 +1,24 @@
+package pe.edu.ulima.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegContacto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RegContacto() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
